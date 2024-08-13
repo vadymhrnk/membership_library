f7011b87d7e01c9667b72edf88de98cbc3e8afba
Vadym Hurnik <vad.com.z@gmail.com>
2024-08-13 14:48:23 +0300

    basic structure, ci script

---
+++ src/main/java/com/example/membershiplibrary/MembershipLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.membershiplibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MembershipLibraryApplication.class, args);
+    }
+
+}
